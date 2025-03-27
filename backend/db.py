71f62faa7d4228d0db8f067e8916e8e6ef69331d
Rahini_Sathish <141403412+RahiniSathish@users.noreply.github.com>
2025-03-27 18:26:38 +0530

    Add the changes

--- backend/db.py
+++ backend/db.py
@@ -9,19 +9,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Create the full path to the database directory one level up
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_FOLDER = os.path.join(BASE_DIR, "..", "database")
 os.makedirs(DB_FOLDER, exist_ok=True)  # Create folder if it doesn't exist
 
-# Full database file path
+
 DB_PATH = os.path.join(DB_FOLDER, "documents.db")
 DATABASE_URL = f"sqlite:///{DB_PATH}"
 
-# SQLAlchemy engine and session
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Declarative base class for models
+
 Base = declarative_base()
 
